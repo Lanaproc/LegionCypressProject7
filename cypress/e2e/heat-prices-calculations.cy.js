3c546b5b9ae30893cb13121c07cffffaf746f16a
Sveta Protsenko <svitlanaprotsenko@gmail.com>
2024-04-18 11:29:26 -0400

    Changes to be committed: 	modified:   cypress/e2e/heat-prices-calculations.cy.js

--- cypress/e2e/heat-prices-calculations.cy.js
+++ cypress/e2e/heat-prices-calculations.cy.js
@@ -4,11 +4,19 @@ import { new_data } from '../fixtures/new_system.json'
 
 describe('Calculation for the current and new heating systems', () => {
 
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
+
     const curr_heater_system_element_name = '[name="heattype"]'
     const floors = '[name="stories"]'
     const square_feet = '[name="squarefeet"]'
     const years = '[name="houseage"]'
     const new_heater_system_element_name = '[name="heatingtype"]'
+    const curr_heating_cost = '[id="CHC"]'
+    const new_heating_cost = '[id="NHC"]'
+    const annual_heting_savings = '[id="AHS"]'
+
 
     it('navigate to the savings calculator page', () => {
         cy.visit('https://consumersenergymanagement.ca/savings-calculator/')
@@ -22,33 +30,30 @@ describe('Calculation for the current and new heating systems', () => {
         cy.get(years).type('1').invoke('val').should('eq', '1')
     })
 
-    
     it('Step 2 - Current heating system', () => {
-        // Works
-        cy.get(curr_heater_system_element_name).select('Current Heater')
-
-        // Select doesn't work. Value changed in dropdown, but error thrown by cy
-        cy.get(curr_heater_system_element_name).select('Natural Gas')
+        data.forEach(element => {
+            cy.get(curr_heater_system_element_name).select(element.system).should('be.visible')
+            cy.get(curr_heating_cost).should('have.value', '')
+        })
     })
 
+    it('Step 2 - New heating system', () => {
+        data.forEach(element => {
+            cy.get(new_heater_system_element_name).select(element.system).should('be.visible')
+            //cy.get(new_heater_system_element_name).select(element.system)
+            cy.get(new_heating_cost).should('have.value', '')
+        })
+    })
+    it('Verify that savings are displayed', () => {
+        cy.get(curr_heater_system_element_name).select('Propane').invoke('text').should('contain', 'Propane')
+        cy.get(new_heater_system_element_name).select('Electric').invoke('text').should('contain', 'Electric')
+        cy.get(annual_heting_savings).should('have.value', '')
+    })
 
-    // it('Step 2 - Current heating system', () => {
-    //     data.forEach(element => {
-    //         cy.get(curr_heater_system_element_name).select(element.system)
-    //     })
-    // })
-
-    // it('Step 2 - New heating system', () => {
-    //     data.forEach(element => {
-    //         cy.get(new_heater_system_element_name).select(element.system)
-    //     })
-    // })
-
-    // it('Step 2 - Current heating system', () => {
-    //     cy.get('#heattype').select('Natural Gas').invoke('text').should('eq', 'Natural Gas')
-    // })
-
-
+    it('Total savinsa update', () => {
+        cy.get('[id="VTAS"]').should('have.value', '')
+        cy.get('[id="VTES"]').should('have.value', '')
+    })
 })
 
 
